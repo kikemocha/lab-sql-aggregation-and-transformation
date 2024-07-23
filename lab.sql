88f380fad23cd942d64e6931d767524d33a40176
kikemocha <enriquemorarey@gmail.com>
2024-07-23 13:45:27 +0200

    versio 1.1

--- lab.sql
+++ lab.sql
@@ -5,7 +5,7 @@ SELECT MAX(`length`) AS max_duration, MIN(`length`) AS min_duration FROM film;
 SELECT CONCAT(FLOOR(AVG(`length`)/60),'h ',ROUND(AVG(`length`)%60,0),'min') FROM film;
 SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) FROM rental;
 SELECT *,DATE_FORMAT(rental_date,'%M') AS Month,DATE_FORMAT(rental_date,'%W') AS weekday FROM rental LIMIT 20;
-SELECT *, IF(DATE_FORMAT(rental_date,'%w') = 7 OR DATE_FORMAT(rental_date,'%w') = 6,'Weekend','Workday') AS DAY_TYPE FROM rental;
+SELECT *, CASE WHEN DATE_FORMAT(rental_date, '%w') IN (0, 6) THEN 'weekend' ELSE 'workday' END AS day_type FROM rental;
 SELECT title, IFNULL(rental_duration, 'Not Available') AS rental_duration FROM film ORDER BY title ASC;
 SELECT CONCAT(first_name,last_name,SUBSTRING(email,1,3)) AS maketing_email FROM customer ORDER BY last_name ASC;
 SELECT COUNT(film_id) FROM film WHERE release_year IS NOT NULL;
